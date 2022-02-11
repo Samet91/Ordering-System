347f8f02176577f6d41aef7f5c47798ab045d3bc
Samet Uslu <samet_u@hotmail.de>
2022-02-11 18:44:48 +0100

    add Hashmap in my constructor

---
+++ src/test/java/de/uslu/ShopServiceTest.java
@@ -0,0 +1,25 @@
+package de.uslu;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopServiceTest {
+
+    @Test
+    void myAddProductShouldBeAddInNewShopService() {
+
+        Product product = new Product("Banane", "1");
+        HashMap<String, Product> map = new HashMap<>();
+        ProductRepo productsRepo = new ProductRepo(map);
+        OrderRepo orders = new OrderRepo();
+
+        ShopService newShopService = new ShopService(orders, productsRepo);
+        newShopService.addProduct(product);
+        var result = newShopService.getProduct("1");
+        assertEquals(product, result);
+    }
+}
